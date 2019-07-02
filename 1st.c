a10a1b95769f4becffb3ad3102857dd0f0412e7a
moyukh00145 <52436728+moyukh00145@users.noreply.github.com>
2019-07-02 08:47:59 +0530

    Add files via upload

---
+++ 1st.c
@@ -0,0 +1,11 @@
+//author : moyukh
+//sum of two numbers
+#include<stdio.h>
+int main()
+{
+    int a,b,c;
+    printf("\nenter the 1st number & 2nd number:");
+    scanf("%d%d",&a,&b);
+    c=a+b;
+    printf("\nsum: %d",c);
+}
