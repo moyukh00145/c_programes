81ea51f3963e7f41249853447ca92cb74d88a9e0
Moyukh  Sarkar <52436728+moyukh00145@users.noreply.github.com>
2019-07-06 12:42:31 +0530

    Add files via upload

---
+++ division and reminder.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+main()
+{
+    int a,b,c;
+    /* a=divisor
+       b=by which divison happens
+       c=reminder*/
+       printf("enter a,b");
+       scanf("%d%d",&a,&b);
+       c=a%b;
+       printf("%d",c);
+}
